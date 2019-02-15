f09a1101e7bd6a44b52ab67d2c6fb584429b56f8
lilian waweru <lilowesh.lw@gmail.com>
2019-02-15 15:30:47 +0300

    equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ fuction findout(){
   var length=parseInt(document.getElementsByid("length")).value
   var Width=parseInt(document.getElementsByid("Width")).value
   var Height=parseInt(document.getElementsByid("Height")).value
+  if(length==width && width==height){
+    alert("EQUILATERAL TrIANGLE")
+  }
 }
