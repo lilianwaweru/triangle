b51502baab1495723c38eaa11c16ef5334b0cf77
lilian waweru <lilowesh.lw@gmail.com>
2019-02-15 15:27:37 +0300

    input variables in js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+fuction findout(){
+  var length=parseInt(document.getElementsByid("length")).value
+  var Width=parseInt(document.getElementsByid("Width")).value
+  var Height=parseInt(document.getElementsByid("Height")).value
+}
