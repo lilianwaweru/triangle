6a962d739cd10078dba17ad3b4ab4c4059f3e184
lilian waweru <lilowesh.lw@gmail.com>
2019-02-15 18:42:13 +0300

    array changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ function out(){
   var sidea=parseInt(document.getElementById('length').value);
   var sideb=parseInt(document.getElementById('width').value);
   var sidec=parseInt(document.getElementById('height').value);
-  var side=[a,b,c]
-  if(a[0]==b[1] && b[1]==c[2] && c[2]==a[0]){
+  var side = [a,b,c];
+  if(side[0]==side[1] && side[1]==side[2] && side[2]==side[0]){
     alert("EQUILATERAL TRIANGLE");
   }
-  else if(a[0]==b[1] || b[1]==c[2] || a[0]==c[2]){
+  else if(side[0]==side[1] || side[1]==side[2] || side[0]==side[2]){
     alert("ISOSCELES TRIANGLE");
   }
-  else if(a[0]!=b[1] && b[1]!=c[2] && a[0]!=c[2]){
+  else if(side[0]!=side[1] && side[1]!=side[2] && side[0]!=side[2]){
     alert("SCALENE TRIANGLE");
   }
   else {
