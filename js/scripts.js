c00c407a0eda4908d46b590dd2f2691d9a1c14ac
lilian waweru <lilowesh.lw@gmail.com>
2019-02-15 16:15:33 +0300

    equilateral in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 function findout(){
-  var a=parseInt(document.getElementsByid("length")).value;
-  var b=parseInt(document.getElementsByid("Width")).value;
-  var c=parseInt(document.getElementsByid("Height")).value;
+  var a=parseInt(document.getElementsByid("length")).value
+  var b=parseInt(document.getElementsByid("Width")).value
+  var c=parseInt(document.getElementsByid("Height")).value
   var triangle = [a,b,c];
-  alert(a)
+  if(triangle[0]==triangle[1] && triangle[1]==triangle[2]){
+    alert("EQUILATERAL TRIANGLE");
+  }
